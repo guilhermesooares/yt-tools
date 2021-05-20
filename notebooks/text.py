5870bc31572ff11c4e255337a00156de2c2bac44
Guilherme Soares <guilhermesoares@dcc.ufmg.br>
2021-05-20 16:14:18 -0300

    Notebook update

--- notebooks/text.py
+++ notebooks/text.py
@@ -9,7 +9,7 @@ def normalize(doc):
     return unidecode(doc)
 
 def remove_stopwords(doc):
-    myStops = ["ta", "ai", "ne"]
+    myStops = ["ta", "ai", "ne", "ia"]
     stop = stopwords.words('portuguese') + myStops
     return ' '.join([word for word in tokenizer.tokenize(doc) if word not in stop and not word.isdigit()])
 
@@ -21,5 +21,5 @@ def clean(doc):
     doc = doc.lower()
     doc = normalize(doc)  # remove acentuacao
     doc = remove_stopwords(doc)
-    doc = apply_stemmer(doc)
+    #doc = apply_stemmer(doc)
     return doc
